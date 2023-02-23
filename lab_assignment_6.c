9efb0ad575f6b9550c6c80774d33f850c5b4c427
Anup Vellanki <vellankianup@gmail.com>
2023-02-23 17:41:43 -0500

    Function Implemented

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,59 +2,75 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+
+   if (low > high) {
+       return -1;
+   }
+
+   int mid = (low + high) / 2;
+
+   if (numbers[mid] == value) {
+       return mid;
+   }
+   else if (numbers[mid] < value) {
+       return search(numbers, mid+1, high, value);
+   }
+   else { 
+       return search(numbers, low, mid-1, value);
+   }
 }
+
 
 void printArray(int numbers[], int sz)
 {
-	int i;
-	printf("Number array : ");
-	for (i = 0;i<sz;++i)
-	{
-		printf("%d ",numbers[i]);
-	}
-	printf("\n");
+    int i;
+    printf("Number array : ");
+    for (i = 0;i<sz;++i)
+    {
+        printf("%d ",numbers[i]);
+    }
+    printf("\n");
 }
 
 int main(void)
 {
-	int i, numInputs;
-	char* str;
-	float average;
-	int value;
-	int index;
-	int* numArray = NULL;
-	int countOfNums;
-	FILE* inFile = fopen("input.txt","r");
+    int i, numInputs;
+    char* str;
+    float average;
+    int value;
+    int index;
+    int* numArray = NULL;
+    int countOfNums;
+    FILE* inFile = fopen("input.txt","r");
 
-	fscanf(inFile, " %d\n", &numInputs);
-	
-	while (numInputs-- > 0)
-	{
-		fscanf(inFile, " %d\n", &countOfNums);
-		numArray = (int *) malloc(countOfNums * sizeof(int));
-		average = 0;
-		for (i = 0; i < countOfNums; i++)
-		{
-			fscanf(inFile," %d", &value);
-			numArray[i] = value;
-			average += numArray[i];
-		}
+    fscanf(inFile, " %d\n", &numInputs);
 
-		printArray(numArray, countOfNums);
-		value = average / countOfNums;
-		index = search(numArray, 0, countOfNums - 1, value);
-		if (index >= 0)
-		{
-			printf("Item %d exists in the number array at index %d!\n",value, index);
-		}
-		else
-		{
-			printf("Item %d does not exist in the number array!\n", value);
-		}
+    while (numInputs-- > 0)
+    {
+        fscanf(inFile, " %d\n", &countOfNums);
+        numArray = (int ) malloc(countOfNums sizeof(int));
+        average = 0;
+        for (i = 0; i < countOfNums; i++)
+        {
+            fscanf(inFile," %d", &value);
+            numArray[i] = value;
+            average += numArray[i];
+        }
 
-		free(numArray);
-	}
+        printArray(numArray, countOfNums);
+        value = average / countOfNums;
+        index = search(numArray, 0, countOfNums - 1, value);
+        if (index >= 0)
+        {
+            printf("Item %d exists in the number array at index %d!\n",value, index);
+        }
+        else
+        {
+            printf("Item %d does not exist in the number array!\n", value);
+        }
 
-	fclose(inFile);
+        free(numArray);
+    }
+
+    fclose(inFile);
 }
